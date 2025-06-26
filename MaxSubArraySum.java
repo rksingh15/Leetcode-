b5fc32282e497063b32a8612f71b2701f83b9b76
Ritik Singh Kushwaha <115486545+rksingh15@users.noreply.github.com>
2025-06-26 10:50:19 +0530

    Update MaxSubArraySum.java

--- MaxSubArraySum.java
+++ MaxSubArraySum.java
@@ -14,3 +14,15 @@ for(int i = 0 ; i<n ; i++){
 	  maxSubArraySum = Math.Max( maxSubArraySum, CurrentSubArraySum);
 }
 }	
+
+Next method  to solve 
+
+	for(int i = 0 ; i<n ; i++){
+			currentSubArraySum=0;
+		for(int j = 0 ; j<n ; j++){
+
+			currentsubArray+=num[k];
+		
+			  maxSubArraySum = Math.Max( maxSubArraySum, CurrentSubArraySum);
+		}
+}
